463380d866a645f47455011de77c2417b70b9917
KichuMohan <kichumohan779@gmail.com>
2020-05-27 23:53:39 +0530

    Updates

---
+++ precache-manifest.8cc4ca8d4786f5f5a516fca08b9f9014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88deb8cfab7dea7d65922f28751195c",
+    "url": "/ig-liker/index.html"
+  },
+  {
+    "revision": "aa013f36250909a4d15b",
+    "url": "/ig-liker/static/css/main.8800a31c.chunk.css"
+  },
+  {
+    "revision": "04629ce874ce2592a2c1",
+    "url": "/ig-liker/static/js/2.9cc0cf03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ig-liker/static/js/2.9cc0cf03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa013f36250909a4d15b",
+    "url": "/ig-liker/static/js/main.b1b5062d.chunk.js"
+  },
+  {
+    "revision": "30d569d4d3a280e8e194",
+    "url": "/ig-liker/static/js/runtime-main.f6558f9a.js"
+  }
+]);
